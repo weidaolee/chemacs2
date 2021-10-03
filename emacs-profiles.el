fbad5fea1911ed73e55bfe73eedecde44ff5dd6b
weidaolee <weidaolee@gmail.com>
2021-10-03 17:45:09 +0800

    go ahead

--- emacs-profiles.el
+++ emacs-profiles.el
@@ -2,4 +2,6 @@
  ("centaur" . ((user-emacs-directory . "~/emacs/centaur")))
  ("doom" . ((user-emacs-directory . "~/emacs/doom")
             (env . (("DOOMDIR" . "~/emacs/doom/doom.d")))))
+ (".doom.backup" . ((user-emacs-directory . "~/emacs/.doom.backup")
+            (env . (("DOOMDIR" . "~/emacs/.doom.backup/doom.d")))))
  )
